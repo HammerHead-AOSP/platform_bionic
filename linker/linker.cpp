2d18e1e4d60e6568dd518799ba3f112d13f652ce
Alice Weiss <admin@2x2ch.ru>
2016-09-24 19:11:34 +0300

    Add arch check

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -3924,12 +3924,14 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 #if !defined(__LP64__)
   if (has_text_relocations) {
     // Fail if app is targeting sdk version > 22
+#if !defined(__i386__)
     if (get_application_target_sdk_version() != __ANDROID_API__
          && get_application_target_sdk_version() > 22) {
       PRINT("%s: has text relocations", get_realpath());
       DL_ERR("%s: has text relocations", get_realpath());
       return false;
     }
+#endif
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied.
     DL_WARN("%s has text relocations. This is wasting memory and prevents "
